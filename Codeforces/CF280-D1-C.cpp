7f6cf420ead9cc8f2518712f9812c436c9b56fe3
shashank0107 <35562709+shashank0107@users.noreply.github.com>
2019-08-28 21:07:17 +0530

    Create CF280-D1-C.cpp

---
+++ Codeforces/CF280-D1-C.cpp
@@ -0,0 +1,77 @@
+/*
+    Author : manu_sy
+    
+    Idea : - Same as editorial of this problem. 
+      https://assets.codeforces.com/statements/280-281/Tutorial.pdf
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+/** Template Begins **/
+
+typedef long long        ll;
+typedef pair<int,int>    PII;
+
+#define IOS         ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl        '\n'
+#define pb          push_back
+#define F           first
+#define S           second
+#define mp          make_pair
+#define FOR(i,a,b)  for(int i=a;i<=b;i++)
+#define LP(i,n)     for(int i=0;i< n;i++)
+
+/* Debug */
+template<class L, class R> ostream &operator<<(ostream &os, pair<L,R> P) {
+  return os << "(" << P.F << "," << P.S << ")";
+} 
+template<class T> ostream &operator<<(ostream& os, vector<T> V) {
+  os << "["; for (auto vv : V) os << vv << ","; return os << "]";
+}
+template<class TH> void _dbg(const char *sdbg, TH h){ cerr<<sdbg<<'='<<h<<endl; }
+template<class TH, class... TA> void _dbg(const char *sdbg, TH h, TA... a) {
+  while(*sdbg!=',')cerr<<*sdbg++;
+  cerr<<'='<<h<<','; _dbg(sdbg+1, a...);
+}
+#ifdef LOCAL
+#define debug(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (__VA_ARGS__)
+#define cerr if(0)cout
+#endif
+/* Debug Ends */
+
+const int N = 1e5+7;
+
+/** Template Ends **/
+int n, dep[N];
+vector<int> g[N];
+bool vis[N];
+
+void dfs(int v, int h) {
+    vis[v] = true;
+    dep[v] = h;
+    for (auto nb : g[v])
+        if (!vis[nb])
+            dfs(nb, h+1);
+}
+
+signed main() {
+
+    IOS;
+    cout << fixed << setprecision(10);
+    cin >> n;
+    LP(i,n-1) {
+        int a, b;   cin >> a >> b;
+        g[a].pb(b);
+        g[b].pb(a);
+    }
+    dfs(1, 1);
+
+    double ans = 0;
+    FOR(i,1,n)
+        ans += 1.0/dep[i];
+    cout << ans << endl;
+
+    return 0;
+}
