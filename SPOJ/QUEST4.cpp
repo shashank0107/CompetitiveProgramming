9abd695fe21aa495040d123219b3208bdc99edaf
shashank0107 <35562709+shashank0107@users.noreply.github.com>
2019-09-11 13:44:18 +0530

    Create QUEST4.cpp

---
+++ SPOJ/QUEST4.cpp
@@ -0,0 +1,119 @@
+/*
+    Author : manu_sy
+    
+    Idea : Make a bipartie graph with all x-axis lines on left and y-axis lines on right.
+           For every marked node (x,y) : make edge x -> y
+           Now goal is to find the minimum no. of vertices such that all the edges are covered (min vertex cover problem).
+           Solution is the max biparite matching of the above graph
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+/** Template Begins **/
+
+typedef long long        ll;
+typedef pair<int,int>    PII;
+
+#define IOS         ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl        '\n'
+#define pb          push_back
+#define F           first
+#define S           second
+#define mp          make_pair
+#define all(a)      (a).begin(), (a).end()
+#define FOR(i,a,b)  for(int i=a;i<=b;i++)
+#define LP(i,n)     for(int i=0;i< n;i++)
+
+/* Debug */
+template<class L, class R> ostream &operator<<(ostream &os, pair<L,R> P) {
+  return os << "(" << P.F << "," << P.S << ")";
+} 
+template<class T> ostream &operator<<(ostream& os, vector<T> V) {
+  os << "["; for (auto vv : V) os << vv << ","; return os << "]";
+}
+template<class TH> void _dbg(const char *sdbg, TH h){ cerr<<sdbg<<'='<<h<<endl; }
+template<class TH, class... TA> void _dbg(const char *sdbg, TH h, TA... a) {
+  while(*sdbg!=',')cerr<<*sdbg++;
+  cerr<<'='<<h<<','; _dbg(sdbg+1, a...);
+}
+#ifdef LOCAL
+#define debug(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (__VA_ARGS__)
+#define cerr if(0)cout
+#endif
+/* Debug Ends */
+
+const int N = 1e5+7;
+
+/** Template Ends **/
+int n;
+
+struct BipartiteMatcher {
+  vector<vector<int>> G;
+  vector<int> L, R, Viz;
+  
+  BipartiteMatcher(int n, int m) :
+  G(n), L(n, -1), R(m, -1), Viz(n) {}
+  
+  void AddEdge(int a, int b) {
+    G[a].push_back(b);
+  }
+  
+  bool Match(int node) {
+    if (Viz[node])
+      return false;
+    Viz[node] = true;
+    
+    for (auto vec : G[node]) {
+      if (R[vec] == -1) {
+        L[node] = vec;
+        R[vec] = node;
+        return true;
+      }
+    }
+    
+    for (auto vec : G[node]) {
+      if (Match(R[vec])) {
+        L[node] = vec;
+        R[vec] = node;
+        return true;
+      }
+    }
+    
+    return false;
+  }
+  
+  int Solve() {
+    int ok = true;
+    while (ok--) {
+      fill(Viz.begin(), Viz.end(), 0);
+      for (int i = 0; i < (int)L.size(); ++i)
+        if (L[i] == -1)
+          ok |= Match(i);
+    }
+    
+    int ret = 0;
+    for (int i = 0; i < L.size(); ++i)
+      ret += (L[i] != -1);
+    return ret;
+  }
+};
+
+
+signed main() {
+
+    IOS;
+    int t;  cin >> t;
+    while (t--) {
+        BipartiteMatcher bm(122, 122);
+        cin >> n;
+        while (n--) {
+            int x, y;   cin >> x >> y;
+            bm.AddEdge(x,y);
+        }
+        cout << bm.Solve() << endl;
+    }
+
+    return 0;
+}
