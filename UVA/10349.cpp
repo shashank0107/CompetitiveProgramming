27a8af6037ea7f53f62772901d579e186b61ce42
Shashank Yadav <35562709+shashank0107@users.noreply.github.com>
2019-09-23 16:00:33 +0530

    Create 10349.cpp

---
+++ UVA/10349.cpp
@@ -0,0 +1,135 @@
+/*
+    Author : manu_sy
+    
+    Idea : Answer is the maximum matching plus the no. of nodes not covered by the maximum matching.
+          For detail :
+          https://github.com/BRAINOOOO/CompetitiveProgramming/blob/master/UVA/V-103/UVA%2010349.Cpp
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+/** Template Begins **/
+
+typedef long long        ll;
+typedef pair<int,int>    PII;
+
+#define IOS         ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl        '\n'
+#define pb          push_back
+#define F           first
+#define S           second
+#define mp          make_pair
+#define all(a)      (a).begin(), (a).end()
+#define FOR(i,a,b)  for(int i=a;i<=b;i++)
+#define LP(i,n)     for(int i=0;i< n;i++)
+
+/* Debug */
+template<class L, class R> ostream &operator<<(ostream &os, pair<L,R> P) {
+  return os << "(" << P.F << "," << P.S << ")";
+} 
+template<class T> ostream &operator<<(ostream& os, vector<T> V) {
+  os << "["; for (auto vv : V) os << vv << ","; return os << "]";
+}
+template<class TH> void _dbg(const char *sdbg, TH h){ cerr<<sdbg<<'='<<h<<endl; }
+template<class TH, class... TA> void _dbg(const char *sdbg, TH h, TA... a) {
+  while(*sdbg!=',')cerr<<*sdbg++;
+  cerr<<'='<<h<<','; _dbg(sdbg+1, a...);
+}
+#ifdef LOCAL
+#define debug(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (__VA_ARGS__)
+#define cerr if(0)cout
+#endif
+/* Debug Ends */
+
+const int N = 1e5+7;
+int dx[] = {0,0,-1,1};
+int dy[] = {1,-1,0,0};
+
+/** Template Ends **/
+char a[50][20];
+int n, m;
+
+struct BipartiteMatcher {
+  vector<vector<int>> G;
+  vector<int> L, R, Viz;
+  
+  BipartiteMatcher(int n, int m) :
+  G(n), L(n, -1), R(m, -1), Viz(n) {}
+  
+  void AddEdge(int a, int b) {
+    G[a].push_back(b);
+  }
+  
+  bool Match(int node) {
+    if (Viz[node])
+      return false;
+    Viz[node] = true;
+    
+    for (auto vec : G[node]) {
+      if (R[vec] == -1) {
+        L[node] = vec;
+        R[vec] = node;
+        return true;
+      }
+    }
+    
+    for (auto vec : G[node]) {
+      if (Match(R[vec])) {
+        L[node] = vec;
+        R[vec] = node;
+        return true;
+      }
+    }
+    
+    return false;
+  }
+  
+  int Solve() {
+    int ok = true;
+    while (ok--) {
+      fill(Viz.begin(), Viz.end(), 0);
+      for (int i = 0; i < (int)L.size(); ++i)
+        if (L[i] == -1)
+          ok |= Match(i);
+    }
+    
+    int ret = 0;
+    for (int i = 0; i < L.size(); ++i)
+      ret += (L[i] != -1);
+    return ret;
+  }
+};
+
+signed main() {
+
+    IOS;
+    int t;  cin >> t;
+    while (t--) {
+        cin >> n >> m;
+
+        BipartiteMatcher bm(n*m, n*m);
+        int cnt = 0;
+        LP(i,n) LP(j,m) {
+            cin >> a[i][j];
+            if (a[i][j] == '*') cnt++;
+        }
+
+        LP(i,n) LP(j,m) if ((i+j)%2 == 0 && a[i][j] == '*') {
+
+            LP(k,4) {
+                int ni = i+dx[k];
+                int nj = j+dy[k];
+                if (ni >= 0 && ni < n && nj >= 0 && nj < m && a[ni][nj] == '*')
+                    bm.AddEdge(i*m+j,ni*m+nj);
+            }
+        }
+        int match = bm.Solve();
+        //debug(cnt, match);
+        int left = cnt - 2*match;
+        cout << left + match << endl;
+    }
+    
+    return 0;
+}
