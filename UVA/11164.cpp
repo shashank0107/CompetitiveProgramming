0c5d6a08c55c5711bbfe9aec8fa5b1a6b7cd1f64
Shashank Yadav <35562709+shashank0107@users.noreply.github.com>
2020-07-15 14:03:38 +0530

    Create 11164.cpp

---
+++ UVA/11164.cpp
@@ -0,0 +1,80 @@
+/*
+    Author : manu_sy
+    Idea : Use formula - https://github.com/rezwan4029/UVA-CODES/blob/master/11164%20-%20Kingdom%20Division.cpp
+*/
+#include "bits/stdc++.h"
+using namespace std;
+ 
+/** Template Begins **/
+ 
+typedef long long        ll;
+typedef pair<int,int>    PII;
+ 
+#define IOS         ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl        '\n'
+#define pb          push_back
+#define F           first
+#define S           second
+#define mp          make_pair
+#define all(a)      (a).begin(), (a).end()
+#define FOR(i,a,b)  for(int i=a;i<=b;i++)
+#define LP(i,n)     for(int i=0;i< n;i++)
+ 
+/* Debug */
+ 
+template<class L, class R> ostream &operator<<(ostream &os, pair<L,R> P) {
+  return os << "(" << P.F << "," << P.S << ")";
+} 
+ 
+template<class T> ostream &operator<<(ostream& os, vector<T> V) {
+  os << "["; for (auto vv : V) os << vv << ","; return os << "]";
+}
+ 
+template<class TH> void _dbg(const char *sdbg, TH h){ cerr << sdbg << '=' << h << endl; }
+ 
+template<class TH, class... TA> void _dbg(const char *sdbg, TH h, TA... a) {
+  while(*sdbg!=',') cerr << *sdbg++;
+  cerr<<'='<<h<<','; _dbg(sdbg+1, a...);
+}
+ 
+#ifdef LOCAL
+#define debug(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (__VA_ARGS__)
+#define cerr if(0)cout
+#endif
+ 
+/* Debug Ends */
+ 
+const int N = 5e6+7;
+ 
+/** Template Ends **/
+double a;
+double b;
+double c;
+ 
+signed main() {
+ 
+    IOS;
+    int t = 1;
+    cout << fixed << setprecision(4);
+    while (cin >> a >> b >> c) {
+    	if (a == -1)	break;
+    	double d1 = (a * c) / b;
+    	cout << "Set " << t++ << ":" << endl;
+    	if (b == 0.0 || b == d1) {
+    		cout << "Poor King!" << endl;
+    		continue;
+    	}
+    	double d2 = ((a + d1) * c + (a + d1) * d1) / (b - d1);
+		double ans = d1 + d2;
+		if (ans > 0) {
+			cout << ans << endl;
+		} else {
+			cout << "Poor King!" << endl;
+		}
+    }
+
+ 
+    return 0;
+}
